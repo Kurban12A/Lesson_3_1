ecfeb97ddf416613ec174ec33f3319034018e54b
Kurban12A <108360687+Kurban12A@users.noreply.github.com>
2022-07-12 22:09:14 +0300

    Update Первая_задача.py

--- Первая_задача.py
+++ Первая_задача.py
@@ -1,13 +1,16 @@
+import datetime
+
 def check_to_date(day, month, year):
-    user_input = list(map(int, input().replace('.', ' ').split()))
+    user_input = list(map(int, input("Введите дату: ").replace('.', ' ').split()))
     for _ in user_input:
         day = user_input[0]
         month = user_input[1]
         year = user_input[2]
 
-        if day <= 31 and day != 0 and month <= 12 and month != 0 and len(str(year)) == 4 and year != 0:
-            return 'OK'
-        else:
-            return 'FAILED'
+    try:
+        datetime.date(day=day, month=month, year=year)
+        return "Correct date"
+    except ValueError:
+        return "Incorrect date"
 
-print(check_to_date(28, 11, 1999))
+print(check_to_date(5, 12, 2020))
